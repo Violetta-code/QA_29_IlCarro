c5592174f35b54665fe5b86401fd3fac8bc789ed
Violetta-code <atteloiv3@gmail.com>
2025-05-04 22:37:09 +0300

    new project ILCarro + Login

---
+++ src/main/java/manager/HelperUser.java
@@ -0,0 +1,31 @@
+package manager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class HelperUser extends HelperBase{
+
+    public HelperUser(WebDriver wd) {
+        super(wd);
+    }
+
+    public void fillLoginForm(String email, String password){
+
+        type(By.xpath("//input[@formcontrolname='email']"), email);
+        type(By.cssSelector("[ng-reflect-name='password']"), password);
+    }
+
+    public void openLoginForm(){
+
+        click(By.cssSelector("a[href='/login?url=%2Fsearch']"));
+    }
+
+    public void submitLogin(){
+        click(By.xpath("//button[text()='Y’alla!']"));
+    }
+
+    public void submitOk(){
+      //  click(By.xpath("//button[text()='Ok']"));
+    }
+}
---
+++ src/test/java/tests/LoginTests.java
@@ -0,0 +1,13 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class LoginTests extends TestBase{
+
+    @Test
+    public void loginTest(){
+        app.getHelperUser().openLoginForm();
+        app.getHelperUser().fillLoginForm("solodka1998@mail.ru","Atteloiv123!");
+        app.getHelperUser().submitLogin();
+    }
+}
